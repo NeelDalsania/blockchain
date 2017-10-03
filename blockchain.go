53c8a7e5d3e4d79254eae6a67f2ce54cea8962f4
NRDa27 <nrda27@gmail.com>
2017-10-04 01:27:43 +0530

    Added NewNode, VerifyChain, FinalBlock, ProofOfWork and ValidProof functions

--- blockchain.go
+++ blockchain.go
@@ -1,5 +1,15 @@
 package blockchain
 
+import {
+    "net/http"
+    "net/url"
+    "time"
+    "fmt"
+    "bytes"
+    "encoding/binary"
+    "encoding/json"
+}
+
 type BlockchainInterface interface {
 
     NewNode(address string) bool // Add a new node to the list of nodes
@@ -20,22 +30,52 @@ type BlockchainInterface interface {
 }
 
 type Block struct {
-
+    Index  int64
+    Timestamp int64
+    Transactions []Transaction
+    Proof int64
+    PreviousHash string
 }
 
 type Transaction struct {
-
+    Sender string
+    Receiver string
+    Amount int64
 }
 
 type Blockchain struct {
-
+    chain []Block
+    transactions []Transaction
+    nodes StringSet
 }
 
 func (bc *Blockchain) NewNode(address string) bool {
+    address, err := url.parse(address)
 
+    if err != nil {
+        return false
+    }
+    return bc.nodes.Add(address.host)
 }
 
-func (bc *Blockchain) VerifyChain(chain Blockchain) bool {
+func (bc *Blockchain) VerifyChain(chain *[]Block) bool {
+    previousBlock := (*chain)[0]
+
+    for currentIndex:=1; currentIndex < len(*chain); currentIndex++ {
+        block := (*chain)[currentIndex]
+
+        if block.PreviousHash != GetHash(preiousBlock) {
+            return false
+        }
+
+        if !bc.ValidProof(previousBlock.Proof, block.Proof) {
+            return false
+        }
+
+        previousBlock = block
+    }
+
+    return true
 
 }
 
@@ -52,9 +92,19 @@ func (bc *Blockchain) RegisterNewTransaction(trans Transaction) int64 {
 }
 
 func (bc *Blockchain) FinalBlock() Block {
-
+    return bc.chain[len(bc.chain)-1]
 }
 
 func (bc *Blockchain) ProofOfWork(lastProof int64) int64 {
+    var proof int64 = 0
+    for !bc.ValidProof(lastproof, proof){
+        proof += 1
+    }
+    return proof
+}
 
+func (bc *Blockchain) ValidProof(lastProof, proof int64) bool {
+    combined := fmt.Sprintf("%d%d", lastProof, proof)
+    computeHash := ComputeHashSha256([]byte(combined))
+    return computeHash[:4] == "0000"
 }
